91357a4c41217431d5c1431fc15cc0398d4a6ab7
Or Elimelech <ore@wix.com>
2014-12-23 13:16:51 +0200

    Service notification on template change

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,19 +9,22 @@ ark 'google-auth-proxy' do
   has_binaries ['google_auth_proxy']
 end
 
+template_vars = {
+  listen_addr: node['gap']['listen_addr'],
+  listen_port: node['gap']['listen_port'],
+  app_name: node['gap']['app_name'],
+  upstreams: node['gap']['upstreams'],
+  pass_basic_auth: node['gap']['pass_basic_auth'],
+  google_apps_domains: node['gap']['google_apps_domains'],
+  google_client_id: node['gap']['google_client_id'],
+  google_client_secret: node['gap']['google_client_secret'],
+  authenticated_emails_file: node['gap']['authenticated_emails_file']
+}
+
 template '/etc/google_auth_proxy.cfg' do
   source 'google_auth_proxy.cfg.erb'
-  variables({
-    listen_addr: node['gap']['listen_addr'],
-    listen_port: node['gap']['listen_port'],
-    app_name: node['gap']['app_name'],
-    upstreams: node['gap']['upstreams'],
-    pass_basic_auth: node['gap']['pass_basic_auth'],
-    google_apps_domains: node['gap']['google_apps_domains'],
-    google_client_id: node['gap']['google_client_id'],
-    google_client_secret: node['gap']['google_client_secret'],
-    authenticated_emails_file: node['gap']['authenticated_emails_file']
-  })
+  variables(template_vars)
+  notifies :restart, 'service[google-auth-proxy]'
 end
 
 include_recipe 'google-auth-proxy::service'
