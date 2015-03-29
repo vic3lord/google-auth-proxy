01097e54cd8a70233e1b7edde793d6aea8a007ef
Or Elimelech <0r3limelech@gmail.com>
2015-03-29 18:16:56 +0300

    Updated to 1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email '0r3limelech@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures google-auth-proxy'
 long_description 'Install/Configures https://github.com/bitly/google_auth_proxy'
-version          '1.0.3'
+version          '1.1.0'
 
 depends 'ark'
